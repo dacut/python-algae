2357fa043a7748babf95f5e9b5392d570550cdd9
David Cuthbert <dacut@kanga.org>
2015-01-06 15:29:34 -0800

    Lowercase PyPI name for consistency.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name="Algae",
+setup(name="algae",
       version="0.1.0",
       description="Python advanced collections library",
       author="David Cuthbert",
