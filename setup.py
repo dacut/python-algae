c368ca91e6bfc93af5d790b50e8d936c28fee011
David Cuthbert <dacut@kanga.org>
2015-01-07 13:54:07 -0800

    Bump revision to 0.1.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="algae",
-      version="0.1.0",
+      version="0.1.1",
       description="Python advanced collections library",
       author="David Cuthbert",
       author_email="dacut@kanga.org",
